3c58867f275813e481f1b915e6469486a3e4b130
Sarah Zhou <syzhou@fb.com>
2016-06-15 14:20:18 -0700

    Initial commit

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Sarah Zhou on 6/15/16.
+//  Copyright © 2016 Sarah Zhou. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Flix/ViewController.swift
+++ Flix/ViewController.swift
@@ -12,6 +12,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let apiKey = "e05d5334e8b56449e07d815578f88efa"
+        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = NSURLRequest(
+            URL: url!,
+            cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
+            timeoutInterval: 10)
+        
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate: nil,
+            delegateQueue: NSOperationQueue.mainQueue()
+        )
+        
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
+                                                                     completionHandler: { (dataOrNil, response, error) in
+                                                                        if let data = dataOrNil {
+                                                                            if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                                                                                data, options:[]) as? NSDictionary {
+                                                                                print("response: \(responseDictionary)")
+                                                                                
+                                                                            }
+                                                                        }
+        })
+        task.resume()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +46,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
 }
 
